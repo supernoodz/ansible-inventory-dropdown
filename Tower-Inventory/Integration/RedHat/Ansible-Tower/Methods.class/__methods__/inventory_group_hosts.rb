953eb59e2800adb5849c486967d66325db18eb26
nik <supernoodz@users.noreply.github.com>
2017-07-21 09:47:38 +0100

    latest

--- Tower-Inventory/Integration/RedHat/Ansible-Tower/Methods.class/__methods__/inventory_group_hosts.rb
+++ Tower-Inventory/Integration/RedHat/Ansible-Tower/Methods.class/__methods__/inventory_group_hosts.rb
@@ -8,20 +8,14 @@ values_hash = {}
 values_hash['!'] = '-- select from list --'
 
 inventory_root_group_ems_ref = $evm.root['dialog_inventory_root_group']
-
-# inventory_root_group_ems_ref = 2
-
 $evm.log(:info, inventory_root_group_ems_ref)
 
 inventory_root_group = $evm.vmdb('ManageIQ_Providers_AutomationManager_InventoryRootGroup').find_by(:ems_ref => inventory_root_group_ems_ref.to_i)
-
-$evm.log(:info, inventory_root_group.inspect)
+# $evm.log(:info, inventory_root_group)
 
 inventory_root_group_id = inventory_root_group.id
-
 configured_systems = $evm.vmdb(:ExtManagementSystem).find_by(:type=>"ManageIQ::Providers::AnsibleTower::AutomationManager").configured_systems
-
-# $evm.log(:info, configured_systems.inspect)
+# $evm.log(:info, configured_systems)
 
 $evm.log(:info, configured_systems.count)
 
